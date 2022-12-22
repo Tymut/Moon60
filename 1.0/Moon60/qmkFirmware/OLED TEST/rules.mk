27cf3ac1a9d8d9af9be1dc049e2d1d356c06793e
Tymut <110722091+Tymut@users.noreply.github.com>
2022-12-22 17:38:53 +0100

    Update rules.mk

--- 1.0/Moon60/qmkFirmware/OLED TEST/rules.mk
+++ 1.0/Moon60/qmkFirmware/OLED TEST/rules.mk
@@ -15,6 +15,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes		# Split keyboard
+LTO_ENABLE = yes        # Memory optimalization
 
 # if firmware needs too much space:
 # LTO_ENABLE = yes
