d7fe3d81ceecaf05c892c1de3be7e6f5a6619a62
Nx0dle <nx0dle@proton.me>
2023-01-11 20:52:40 +0100

    product usb info moved

--- 1.0/Moon60/qmkFirmware/OLED TEST/config.h
+++ 1.0/Moon60/qmkFirmware/OLED TEST/config.h
@@ -2,12 +2,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0001
-#define DEVICE_VER   0x0001
-#define PRODUCT      moon60
-
 /* key matrix size */
 // Rows are doubled-up
 #define MATRIX_ROWS  10
