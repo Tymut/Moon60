b1214d7c350df016ec0cdf0ec50615c40830b779
Nx0dle <nx0dle@proton.me>
2023-03-04 00:14:56 +0100

    via implementation

---
+++ 1.0/Moon60/qmkFirmware/moon60/keymaps/via/config.h
@@ -0,0 +1,3 @@
+#pragma once
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
---
+++ 1.0/Moon60/qmkFirmware/moon60/keymaps/via/rules.mk
@@ -0,0 +1,13 @@
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE = no     # Enable one-hand typing
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+SPLIT_KEYBOARD = yes
+VIA_ENABLE = yes
