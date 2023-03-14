35b1d3a9b302003ae5baf000cafcebffad35ebae
Nx0dle <nx0dle@proton.me>
2023-03-14 07:00:45 +0100

    test merge

--- 1.0/Moon60/qmkFirmware/moon60/keymaps/test/rules.mk
+++ 1.0/Moon60/qmkFirmware/moon60/keymaps/test/rules.mk
@@ -1,9 +1,7 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+MOUSEKEY_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no
+NKRO_ENABLE = yes
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
@@ -11,3 +9,7 @@ SWAP_HANDS_ENABLE = no     # Enable one-hand typing
 LTO_ENABLE = yes
 OLED_ENABLE = yes
 SPLIT_KEYBOARD = yes
+CONSOLE_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+
+WPM_ENABLE = yes
