5a9cf74663d23d45c3efa05bd44b8367b2f55887
Nx0dle <nx0dle@proton.me>
2024-06-18 22:11:24 +0200

    repository reorganize - remove old files

--- 1.0/Moon60/qmkFirmware/moon60/keymaps/default/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-
-#define OLED_TIMEOUT 120000
-#define OLED_BRIGHTNESS 120
--- 1.0/Moon60/qmkFirmware/moon60/keymaps/test/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-
-#define OLED_TIMEOUT 180000
